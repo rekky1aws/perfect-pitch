8aa789d385b94fc1e8c011a4504f40791e653ce7
rekky1aws <killikomfr@gmail.com>
2022-07-31 00:34:57 +0200

    Button to play a sound.

--- dist/script/app.js
+++ dist/script/app.js
@@ -1,1 +1,15 @@
 // Pitch game
+var play_button = document.querySelector('#play-button');
+play_button === null || play_button === void 0 ? void 0 : play_button.addEventListener('click', playSound);
+function playSound() {
+    var context = new AudioContext();
+    var o = context.createOscillator();
+    var g = context.createGain();
+    o.frequency.value = (Math.random() * 500) + 50;
+    o.connect(g);
+    g.connect(context.destination);
+    o.start(0);
+    g.gain.exponentialRampToValueAtTime(0.00001, context.currentTime + 2);
+    console.log(o);
+}
+console.log("chargé");
--- src/script/app.ts
+++ src/script/app.ts
@@ -1,2 +1,20 @@
 // Pitch game
+const play_button = document.querySelector('#play-button');
+
+play_button?.addEventListener('click', playSound);
+
+function playSound (): void {
+	var context = new AudioContext()
+	var o = context.createOscillator()
+	var  g = context.createGain()
+	o.frequency.value = (Math.random() * 500) + 50;
+	o.connect(g)
+	g.connect(context.destination)
+	o.start(0);
+
+	g.gain.exponentialRampToValueAtTime(0.00001, context.currentTime + 2)
+	console.log(o);
+}
+
+console.log("chargé");
 
