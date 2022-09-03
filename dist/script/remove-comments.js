4b7cfe8b1d5f1bfd50463d85f996b38d66376aa0
= <killikomfr@gmail.com>
2022-09-04 01:21:09 +0200

    Documentation for remove-comments.ts.
    Comments in remove-comments.ts to explain how to use it.

--- dist/script/remove-comments.js
+++ dist/script/remove-comments.js
@@ -1,3 +1,9 @@
+/*
+    This script removes all the comments in the original
+    HTML code.
+    It should be placed before any other script tag with
+    a defer attribute.
+*/
 // Removes all comments from the HTML file.
 document.body.innerHTML = document.body.innerHTML.replace(/<!--[\s\S]*?-->/g, "");
 // Removes the tag that calls this script.
--- src/script/remove-comments.ts
+++ src/script/remove-comments.ts
@@ -1,6 +1,14 @@
+/*
+	This script removes all the comments in the original
+	HTML code.
+	It should be placed before any other script tag with
+	a defer attribute.
+*/
+
 // Removes all comments from the HTML file.
 document.body.innerHTML = document.body.innerHTML.replace(/<!--[\s\S]*?-->/g, "");
 
 // Removes the tag that calls this script.
 const removeCommentsSrc = document.querySelector('#remove-comments-src');
 removeCommentsSrc.parentNode.removeChild(removeCommentsSrc);
+
