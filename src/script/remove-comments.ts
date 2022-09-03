233d798c88fb5d60c2fd8fd55a39614d8de4845c
= <killikomfr@gmail.com>
2022-09-04 01:06:20 +0200

    Added ascript to remove HTML comments.

---
+++ src/script/remove-comments.ts
@@ -0,0 +1,6 @@
+// Removes all comments from the HTML file.
+document.body.innerHTML = document.body.innerHTML.replace(/<!--[\s\S]*?-->/g, "");
+
+// Removes the tag that calls this script.
+const removeCommentsSrc = document.querySelector('#remove-comments-src');
+removeCommentsSrc.parentNode.removeChild(removeCommentsSrc);
